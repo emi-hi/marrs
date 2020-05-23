c4ea1d798f6c3986ddac064a203532dbb8fac7b4
Emily <emily.hillier@gmail.com>
2020-05-22 22:42:19 -0700

    changed home text to h5

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,14 +9,21 @@ export default function Home(props) {
       <div className="home-row">
         <img id="astro-ted" alt="astronaut Ted" src="images/astro-large.jpg"/>
         <div className="notice">
+          <h5>
           COVID-19 Notice 
-          <br/>
+          </h5>
+          <h6>
+
           The shop is currently open by appointment only. Please browse the products online and give us a call if you'd like to come in for a particular one. Please also call ahead if you would like to drop off a repair or consignment item. 
+          </h6>
         </div>
       </div>
       <div className="featured-items">
+        <h5>
+
         Featured Items
-        
+        </h5>
+
       </div>
     </div>
   )
